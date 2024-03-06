977ac3b32649e3059e43ac3645b3bcf6c5c8e74c
Sai Koushik <koushik.vanama@gmail.com>
2024-03-06 20:01:31 +0530

    pyramid star pattern

---
+++ pyramid.js
@@ -0,0 +1,22 @@
+function drawPyramid(n) {
+    let string = "";
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= n - i; j++) {
+            string += " ";
+        }
+        for (let k = 0; k < 2 * i - 1; k++) {
+            string += "*";
+        }
+        string += "\n";
+    }
+    return string;
+}
+
+console.log(drawPyramid(6));
+// output:
+//         *
+//        ***
+//       *****
+//      *******
+//     *********
+//    ***********
